f80094847fd1a2287bbcf575d0a477101f2bbddc
Rosie-g <ruz0408@gmail.com>
2020-11-10 09:49:40 -0800

    project is shared in remote repo, commiting more changes

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,9 @@
 public class Hello {
    //more staff
     // Mac users and windows users are happy to see commit dialog finally
+
+    // i have already published on github.com
+    // now i am ready to make commits
+    // but do i need to share again?
+    // absolutely no
 }
